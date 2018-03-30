050330b9c6b8c55868392d778c03570fc4f0ac0e
LilqnaDimitrova <37212531+LilqnaDimitrova@users.noreply.github.com>
2018-03-30 15:06:15 +0300

    Update Problem_1.R

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,30 @@
+########Problem 1#########
+#a)
+x<-c(4,1,1,4)
+
+#b)
+y<-c(1,4)
+
+#c)
+x-y
+# We have two vectors of different size.The smaller vector becomes c(1,4,1,4).That's why the result looks like this.
+
+#d)
+s<-c(x,y)
+
+#e)
+rep(s,10)
+srep<-rep(s,10)
+length(srep)
+
+#f)
+rep(s,each=10)
+
+#g)
+seq(7,21,by=1)
+
+7:21
+
+#h)lseq<-seq(7,21,by=1)
+length(lseq)
 
